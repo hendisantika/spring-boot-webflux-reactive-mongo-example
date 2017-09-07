30d639b3fcecb068e8543057335be8e4f96585f4
Hendi Santika <hendisantika@gmail.com>
2017-09-07 21:43:00 +0700

    #6 - Added SecurityConfiguration.java

---
+++ src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootwebfluxreactivemongoexample.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.MapUserDetailsRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsRepository;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/7/17
+ * Time: 9:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    UserDetailsRepository userDetailsRepository() {
+        UserDetails hendi = withUsername("hendi").password("password").roles("USER").build();
+        UserDetails santika = withUsername("hendi").password("password").roles("USER", "ADMIN").build();
+        return new MapUserDetailsRepository(hendi, santika);
+    }
+}
