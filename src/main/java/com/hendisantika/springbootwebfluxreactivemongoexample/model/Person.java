bf346107bf00413f2a5c0858667cd4df76c76ee2
Hendi Santika <hendisantika@gmail.com>
2017-09-08 05:51:40 +0700

    #14 - Modified Person.java

--- src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/model/Person.java
+++ src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/model/Person.java
@@ -1,11 +1,8 @@
 package com.hendisantika.springbootwebfluxreactivemongoexample.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Entity;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,14 +14,52 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * To change this template use File | Settings | File Templates.
  */
 
-@Document
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+@Entity
 public class Person {
-    private String name;
-    private int age;
     @Id
     private String id;
+    private String name;
+    private int age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public Person(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
