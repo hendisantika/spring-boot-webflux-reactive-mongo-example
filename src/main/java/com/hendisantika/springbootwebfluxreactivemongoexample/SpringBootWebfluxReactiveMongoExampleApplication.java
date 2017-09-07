96934ec00e9f31a1d6d34857d1504eb523953336
Hendi Santika <hendisantika@gmail.com>
2017-09-07 21:26:36 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/SpringBootWebfluxReactiveMongoExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootwebfluxreactivemongoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxReactiveMongoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxReactiveMongoExampleApplication.class, args);
+	}
+}
