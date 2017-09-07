907f2399aa825e75df379362e5bd95090677dd6c
Hendi Santika <hendisantika@gmail.com>
2017-09-07 21:46:11 +0700

    #8 - Added PersonController.java

---
+++ src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/controller/PersonController.java
@@ -0,0 +1,31 @@
+package com.hendisantika.springbootwebfluxreactivemongoexample.controller;
+
+import com.hendisantika.springbootwebfluxreactivemongoexample.model.Person;
+import com.hendisantika.springbootwebfluxreactivemongoexample.repository.PersonRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/7/17
+ * Time: 9:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+    @Autowired
+    private PersonRespository personRespository;
+
+    @GetMapping
+    public Flux<Person> index() {
+        return personRespository.findAll();
+    }
+}
