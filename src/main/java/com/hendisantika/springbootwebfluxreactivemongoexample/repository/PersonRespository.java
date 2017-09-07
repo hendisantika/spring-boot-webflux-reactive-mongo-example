21d1f5447e73f91cbbdfdee73662761b932f2578
Hendi Santika <hendisantika@gmail.com>
2017-09-07 22:04:47 +0700

    #11 - Modified PersonRepository

--- src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/repository/PersonRespository.java
+++ src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/repository/PersonRespository.java
@@ -1,8 +1,10 @@
 package com.hendisantika.springbootwebfluxreactivemongoexample.repository;
 
 import com.hendisantika.springbootwebfluxreactivemongoexample.model.Person;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,4 +17,11 @@ import reactor.core.publisher.Flux;
  */
 public interface PersonRespository extends ReactiveMongoRepository<Person, String>{
     Flux<Person> findByName(String name);
+
+    @Query("{ 'name': ?0, 'age': ?1}")
+    Mono<Person> findBynameAndAge(String firstname, int age);
+
+    // Accept parameter inside a reactive type for deferred execution
+    Flux<Person> findByAge(Mono<Integer> age);
+
 }
