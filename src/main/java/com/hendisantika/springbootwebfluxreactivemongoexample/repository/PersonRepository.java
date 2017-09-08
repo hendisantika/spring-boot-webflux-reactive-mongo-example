a898c38c0b93898d58536a13099a762c7141140e
Hendi Santika <hendisantika@gmail.com>
2017-09-08 07:37:40 +0700

    #19 - Added more repo

--- src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/repository/PersonRepository.java
+++ src/main/java/com/hendisantika/springbootwebfluxreactivemongoexample/repository/PersonRepository.java
@@ -1,10 +1,8 @@
 package com.hendisantika.springbootwebfluxreactivemongoexample.repository;
 
 import com.hendisantika.springbootwebfluxreactivemongoexample.model.Person;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,48 +15,50 @@ import reactor.core.publisher.Mono;
  */
 
 public interface PersonRepository extends ReactiveMongoRepository<Person, String>{
-    Flux<Person> findByName(String name);
+    Flux<Person> findByFirstName(String name);
 
-    @Query("{ 'name': ?0, 'age': ?1}")
-    Mono<Person> findBynameAndAge(String firstname, int age);
-
-    // Accept parameter inside a reactive type for deferred execution
-    Flux<Person> findByAge(Mono<Integer> age);
-
-    /**
-     * Derived query selecting by {@code lastname}.
-     *
-     * @param lastname
-     * @return
-     */
-    Flux<Person> findByLastname(String lastname);
-
-    /**
-     * String query selecting one entity.
-     *
-     * @param lastname
-     * @return
-     */
-    @Query("{ 'firstname': ?0, 'lastname': ?1}")
-    Mono<Person> findByFirstnameAndLastname(String firstname, String lastname);
-
-    /**
-     * Derived query selecting by {@code lastname}. {@code lastname} uses deferred resolution that does not require
-     * blocking to obtain the parameter value.
-     *
-     * @param lastname
-     * @return
-     */
-    Flux<Person> findByLastname(Mono<String> lastname);
-
-    /**
-     * Derived query selecting by {@code firstname} and {@code lastname}. {@code firstname} uses deferred resolution that
-     * does not require blocking to obtain the parameter value.
-     *
-     * @param firstname
-     * @param lastname
-     * @return
-     */
-    Mono<Person> findByFirstnameAndLastname(Mono<String> firstname, String lastname);
+    Flux<Person> findOneByFirstName(String name);
+//
+//    @Query("{ 'firstName': ?0, 'age': ?1}")
+//    Mono<Person> findByFirstNameAndAge(String firstname, int age);
+//
+//    // Accept parameter inside a reactive type for deferred execution
+//    Flux<Person> findByAge(Mono<Integer> age);
+//
+//    /**
+//     * Derived query selecting by {@code lastname}.
+//     *
+//     * @param lastname
+//     * @return
+//     */
+//    Flux<Person> findByLastname(String lastname);
+//
+//    /**
+//     * String query selecting one entity.
+//     *
+//     * @param lastname
+//     * @return
+//     */
+//    @Query("{ 'firstName': ?0, 'lastName': ?1}")
+//    Mono<Person> findByFirstnameAndLastname(String firstname, String lastname);
+//
+//    /**
+//     * Derived query selecting by {@code lastname}. {@code lastname} uses deferred resolution that does not require
+//     * blocking to obtain the parameter value.
+//     *
+//     * @param lastname
+//     * @return
+//     */
+//    Flux<Person> findByLastname(Mono<String> lastname);
+//
+//    /**
+//     * Derived query selecting by {@code firstname} and {@code lastname}. {@code firstname} uses deferred resolution that
+//     * does not require blocking to obtain the parameter value.
+//     *
+//     * @param firstname
+//     * @param lastname
+//     * @return
+//     */
+//    Mono<Person> findByFirstnameAndLastname(Mono<String> firstname, String lastname);
 
 }
